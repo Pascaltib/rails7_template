88c719bfc2bc9d47e1a30c0fcfadb71e7fc99320
Pascal Tiberghien <41559804+Pascaltib@users.noreply.github.com>
2022-10-26 12:38:09 +0200

    Finalized rails template

---
+++ rails_template.rb
@@ -0,0 +1,99 @@
+run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
+
+# Gemfile
+########################################
+
+inject_into_file "Gemfile", after: 'gem "debug", platforms: %i[ mri mingw x64_mingw ]' do
+<<-RUBY
+
+  gem "dotenv-rails"
+RUBY
+end
+
+# Uncomment sassc gem
+gsub_file("Gemfile", '# gem "sassc-rails"', 'gem "sassc-rails"')
+
+# Assets
+########################################
+run "rm -rf app/assets/stylesheets"
+run "rm -rf vendor"
+run "curl -L https://github.com/Pascaltib/templates/blob/main/rails-stylesheet.zip > stylesheets.zip"
+run "unzip stylesheets.zip -d app/assets && rm -f stylesheets.zip && rm -f app/assets/rails-stylesheet/README.md"
+run "mv app/assets/rails-stylesheet app/assets/stylesheets"
+
+inject_into_file "config/initializers/assets.rb", before: "# Precompile additional assets." do
+  <<~RUBY
+    Rails.application.config.assets.paths << Rails.root.join("node_modules")
+  RUBY
+end
+
+# Layout
+########################################
+
+gsub_file(
+  "app/views/layouts/application.html.erb",
+  '<meta name="viewport" content="width=device-width,initial-scale=1">',
+  '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
+)
+
+# README
+########################################
+markdown_file_content = <<~MARKDOWN
+  Rails app generated with Pascal Tiberghien's Rails template
+MARKDOWN
+file "README.md", markdown_file_content, force: true
+
+# Generators
+########################################
+generators = <<~RUBY
+  config.generators do |generate|
+    generate.assets false
+    generate.helper false
+    generate.test_framework :test_unit, fixture: false
+  end
+RUBY
+
+environment generators
+
+########################################
+# After bundle
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command "db:drop db:create db:migrate"
+  generate(:controller, "pages", "home", "--skip-routes", "--no-test-framework")
+
+  # Routes
+  ########################################
+  route 'root to: "pages#home"'
+
+  # Gitignore
+  ########################################
+  append_file ".gitignore", <<~TXT
+    # Ignore .env file containing credentials.
+    .env*
+
+    # Ignore Mac and Linux file system files
+    *.swp
+    .DS_Store
+  TXT
+
+  
+  # Heroku
+  run "bundle lock --add-platform x86_64-linux"
+
+  # Dotenv
+  ########################################
+  run "touch '.env'"
+
+  # Rubocop
+  ########################################
+  run "curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml"
+
+  # Git
+  ########################################
+  git :init
+  git add: "."
+  git commit: "-m 'Initial commit for pascaltib's rails template'"
+end
